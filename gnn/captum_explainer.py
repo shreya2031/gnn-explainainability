710c941efbd0bf062958e312fdafc566f348e22f
Shreya <42346881+shreya2031@users.noreply.github.com>
2024-03-23 16:23:50 -0700

    Fix bugs

--- gnn/captum_explainer.py
+++ gnn/captum_explainer.py
@@ -19,130 +19,7 @@ from torch_geometric.nn import SAGEConv, to_hetero
 
 from relbench.external.graph import get_node_train_table_input, make_pkey_fkey_graph
 
-MOVIE_HEADERS = [
-    "movieId", "title", "releaseDate", "videoReleaseDate", "IMDb URL",
-    "unknown", "Action", "Adventure", "Animation", "Children's", "Comedy",
-    "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror",
-    "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"
-]
-USER_HEADERS = ["userId", "age", "gender", "occupation", "zipCode"]
-RATING_HEADERS = ["userId", "movieId", "rating", "timestamp"]
 
-
-class MovieLens(InMemoryDataset):
-    url = 'https://files.grouplens.org/datasets/movielens/ml-100k.zip'
-
-    def __init__(
-        self,
-        root: str,
-        transform: Optional[Callable] = None,
-        pre_transform: Optional[Callable] = None,
-        force_reload: bool = False,
-    ) -> None:
-        super().__init__(root, transform, pre_transform,
-                         force_reload=force_reload)
-        self.load(self.processed_paths[0], data_cls=HeteroData)
-
-    @property
-    def raw_file_names(self) -> List[str]:
-        return ['u.item', 'u.user', 'u1.base', 'u1.test']
-
-    @property
-    def processed_file_names(self) -> str:
-        return 'data.pt'
-
-    def download(self) -> None:
-        path = download_url(self.url, self.root)
-        extract_zip(path, self.root)
-        os.remove(path)
-        folder = osp.join(self.root, 'ml-100k')
-        fs.rm(self.raw_dir)
-        os.rename(folder, self.raw_dir)
-
-    def process(self) -> None:
-        import pandas as pd
-
-        data = HeteroData()
-
-        # Process movie data:
-        df = pd.read_csv(
-            self.raw_paths[0],
-            sep='|',
-            header=None,
-            names=MOVIE_HEADERS,
-            index_col='movieId',
-            encoding='ISO-8859-1',
-        )
-        movie_mapping = {idx: i for i, idx in enumerate(df.index)}
-
-        x = df[MOVIE_HEADERS[6:]].values
-        data['movie'].x = torch.from_numpy(x).to(torch.float)
-
-        # Process user data:
-        df = pd.read_csv(
-            self.raw_paths[1],
-            sep='|',
-            header=None,
-            names=USER_HEADERS,
-            index_col='userId',
-            encoding='ISO-8859-1',
-        )
-        user_mapping = {idx: i for i, idx in enumerate(df.index)}
-
-        age = df['age'].values / df['age'].values.max()
-        age = torch.from_numpy(age).to(torch.float).view(-1, 1)
-
-        gender = df['gender'].str.get_dummies().values
-        gender = torch.from_numpy(gender).to(torch.float)
-
-        occupation = df['occupation'].str.get_dummies().values
-        occupation = torch.from_numpy(occupation).to(torch.float)
-
-        data['user'].x = torch.cat([age, gender, occupation], dim=-1)
-
-        # Process rating data for training:
-        df = pd.read_csv(
-            self.raw_paths[2],
-            sep='\t',
-            header=None,
-            names=RATING_HEADERS,
-        )
-
-        src = [user_mapping[idx] for idx in df['userId']]
-        dst = [movie_mapping[idx] for idx in df['movieId']]
-        edge_index = torch.tensor([src, dst])
-        data['user', 'rates', 'movie'].edge_index = edge_index
-
-        rating = torch.from_numpy(df['rating'].values).to(torch.long)
-        data['user', 'rates', 'movie'].rating = rating
-
-        time = torch.from_numpy(df['timestamp'].values)
-        data['user', 'rates', 'movie'].time = time
-
-        data['movie', 'rated_by', 'user'].edge_index = edge_index.flip([0])
-        data['movie', 'rated_by', 'user'].rating = rating
-        data['movie', 'rated_by', 'user'].time = time
-
-        # Process rating data for testing:
-        df = pd.read_csv(
-            self.raw_paths[3],
-            sep='\t',
-            header=None,
-            names=RATING_HEADERS,
-        )
-
-        src = [user_mapping[idx] for idx in df['userId']]
-        dst = [movie_mapping[idx] for idx in df['movieId']]
-        edge_label_index = torch.tensor([src, dst])
-        data['user', 'rates', 'movie'].edge_label_index = edge_label_index
-
-        edge_label = torch.from_numpy(df['rating'].values).to(torch.float)
-        data['user', 'rates', 'movie'].edge_label = edge_label
-
-        if self.pre_transform is not None:
-            data = self.pre_transform(data)
-
-        self.save([data], self.processed_paths[0])
 
 class MovieLensData(InMemoryDataset):
 
@@ -218,6 +95,7 @@ class MovieLensData(InMemoryDataset):
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 dataset = MovieLensData(root='./movie_lens')
+# Heterodata object
 data = dataset[0].to(device)
 
 # Add user node features for message passing:
@@ -239,10 +117,6 @@ data, _, _ = T.RandomLinkSplit(
     rev_edge_types=[('movie', 'rev_rates', 'user')],
 )(data)
 
-
-print("movie_num_nodes",data['movie'].num_nodes)
-print("user_num_nodes",data['user'].num_nodes)
-print("num_edges",data['user', 'rates', 'movie'].num_edges)
 
 
 
@@ -397,10 +271,10 @@ explanation_gbp = explainer_gbp(
 )
 
 
-path1 = './results/feature_importance_IntegratedGradients.png'
-path2 = './results/feature_importance_saliency.png'
-path3 = './results/feature_importance_Deconvolution.png'
-path4 = './results/feature_importance_GuidedBackprop.png'
+path1 = 'feature_importance_IntegratedGradients.png'
+path2 = 'feature_importance_saliency.png'
+path3 = 'feature_importance_Deconvolution.png'
+path4 = 'feature_importance_GuidedBackprop.png'
 
 # Visualize the feature importance
 explanation_ig.visualize_feature_importance(path1, top_k=10)
